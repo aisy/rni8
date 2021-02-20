2b34dd04de68713989456e16abf332683d0aa505
aisy <aisymuhammad@gmail.com>
2021-02-20 21:22:24 +0700

    add new page

---
+++ src/pages/Config.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Config = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Config</Text>
+    </View>
+  );
+}
+
+export default Config
